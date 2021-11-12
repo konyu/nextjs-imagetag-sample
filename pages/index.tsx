206d8900569a0b0a0636eb519bfaa2961f952fa0
kon_yu <yusukedrive@gmail.com>
2021-11-12 12:53:45 +0900

    Fix: image ratio

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,12 +25,12 @@ export default function Home({
       </Head>
 
       <div className={styles.hero} >
-        <Image src={"/photo-1620050649633-1f9a8c981346.jpeg"} width={1235 } height={823} layout="responsive"/>
+        <Image src={"/photo-1620050649633-1f9a8c981346.jpeg?ar=12:9&fit=crop"} width={1200} height={900} layout="responsive"/>
       </div>
 
       <section className={utilStyles.headingMd}>
         <h1 className={styles.h1} >タイトル</h1>
-        <Image src={"/photo-1620050649633-1f9a8c981346.jpeg"} width={200} height={200} layout="intrinsic"/>
+        <Image src={"/photo-1620050649633-1f9a8c981346.jpeg?ar=1:1&fit=crop"} width={200} height={200} layout="fixed"/>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -52,27 +52,27 @@ export default function Home({
       <div className={styles.cssgrid}>
         <div>
             <h3>アイテム1</h3>
-            <Image src={"/photo-1620050649633-1f9a8c981346.jpeg"} width={600} height={400} layout="fixed"/>
+            <Image src={"/photo-1620050649633-1f9a8c981346.jpeg?ar=3:2&fit=crop"} width={600} height={400} layout="responsive"/>
             <p>適当なテキストメッセージ。適当なテキストメッセージ。適当なテキストメッセージ。適当なテキストメッセージ。適当なテキストメッセージ。適当なテキストメッセージ。適当なテキストメッセージ。適当なテキストメッセージ。適当なテキストメッセージ。</p>
         </div>
         <div className={styles.item1}>
             <h3>アイテム2</h3>
-            <Image src={"/photo-1620050649633-1f9a8c981346.jpeg"} width={600} height={400} layout="responsive"/>
+            <Image src={"/photo-1620050649633-1f9a8c981346.jpeg?ar=3:2&fit=crop"} width={600} height={400} layout="responsive"/>
             <p>適当なテキストメッセージ。</p>
         </div>
         <div className={styles.item1}>
             <h3>アイテム3</h3>
-            <Image src={"/photo-1620050649633-1f9a8c981346.jpeg"} width={600} height={400} layout="responsive"/>
+            <Image src={"/photo-1620050649633-1f9a8c981346.jpeg?ar=3:2&fit=crop"} width={600} height={400} layout="responsive"/>
             <p>適当なテキストメッセージ。</p>
         </div>
         <div className={styles.item1}>
             <h3>アイテム4</h3>
-            <Image src={"/photo-1620050649633-1f9a8c981346.jpeg"} width={600} height={400} layout="responsive"/>
+            <Image src={"/photo-1620050649633-1f9a8c981346.jpeg?ar=3:2&fit=crop"} width={600} height={400} layout="responsive"/>
             <p>適当なテキストメッセージ。</p>
         </div>
         <div className={styles.item1}>
             <h3>アイテム5</h3>
-            <Image src={"/photo-1620050649633-1f9a8c981346.jpeg"} width={600} height={400} layout="responsive"/>
+            <Image src={"/photo-1620050649633-1f9a8c981346.jpeg?ar=3:2&fit=crop"} width={600} height={400} layout="responsive"/>
             <p>適当なテキストメッセージ。</p>
         </div>
       </div>
